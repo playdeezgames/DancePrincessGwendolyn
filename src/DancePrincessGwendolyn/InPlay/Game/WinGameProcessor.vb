2fc17da6ada9d8226266359fb40d2a6749ed1733
TheGrumpyGameDev <ernestpazera@gmail.com>
2022-07-31 05:24:29 -0500

    forgot the win sound

--- src/DancePrincessGwendolyn/InPlay/Game/WinGameProcessor.vb
+++ src/DancePrincessGwendolyn/InPlay/Game/WinGameProcessor.vb
@@ -3,6 +3,7 @@
         AnsiConsole.Clear()
         AnsiConsole.MarkupLine($"{character.Name} has put on the {ItemType.DiamondTiara.Name} and become the new {CharacterType.DanceQueen.Name}!")
         AnsiConsole.MarkupLine($"Congratulations!")
+        SfxPlayer.Play(Sfx.Win)
         OkPrompt()
     End Sub
 End Module
